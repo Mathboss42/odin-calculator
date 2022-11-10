46a191e36574351db566277028c2e746b9b51781
Mathboss42 <115639944+Mathboss42@users.noreply.github.com>
2022-11-10 22:28:16 +0100

    Setup basic calculator functionalities

---
+++ script.js
@@ -0,0 +1,117 @@
+let displayValue = '';
+let firstValue = '';
+let secondValue = '';
+let currentOperator = '';
+
+
+const displayScreen = document.querySelector('p');
+const operandButtons = Array.from(document.querySelectorAll('.operand'));
+operandButtons.forEach(button => button.addEventListener('click', display));
+const operatorButtons = Array.from(document.querySelectorAll('.operator'));
+operatorButtons.splice(3, 2);
+operatorButtons.forEach(button => button.addEventListener('click', setOperator));
+const equalsButton = document.querySelector('[data-key="="]');
+equalsButton.addEventListener('click', equals);
+const clearButton = document.querySelector('[data-key="clear"]');
+clearButton.addEventListener('click', clear);
+
+
+//when user presses operator, store display value in temporary variable and reset display value
+//store operator value in variable
+//when user presses new numbers, store those in displayValue
+//when user presses equals, operate and display result
+
+
+function display(event) {
+    displayValue += event.target.dataset.key;
+    displayScreen.textContent = displayValue;
+    if (firstValue !== '' && secondValue === '') {
+        secondValue = displayValue;
+        // console.log(firstValue);
+        // console.log(secondValue);
+        // console.log(displayValue);
+        // console.log(currentOperator);
+    }
+}
+
+
+function updateDisplay() {
+    displayScreen.textContent = displayValue;
+}
+
+
+function setOperator(event) {
+    if (firstValue === '' && secondValue === '') {
+        firstValue = displayValue;
+        displayValue = '';
+        currentOperator = event.target.dataset.key;
+        // console.log(firstValue);
+        // console.log(secondValue);
+        // console.log(displayValue);
+        // console.log(currentOperator);
+    } else if (firstValue !== '' && secondValue !== '') {
+        equals();
+        currentOperator = event.target.dataset.key;
+        firstValue = displayValue;
+        secondValue = '';
+        displayValue = '';
+        // console.log(firstValue);
+        // console.log(secondValue);
+        // console.log(displayValue);
+        // console.log(currentOperator);
+    } 
+}
+
+
+function equals() {
+    displayValue = operate(firstValue, secondValue, currentOperator);
+    updateDisplay();
+}
+
+
+function clear() {
+    displayValue = '';
+    firstValue = '';
+    secondValue = '';
+    updateDisplay();
+}
+
+
+function add(a, b) {
+    return Number(a) + Number(b);
+}
+
+
+function subtract(a, b) {
+    return a - b;
+}
+
+
+function multiply(a, b) {
+    return a * b; 
+}
+
+
+function divide(a, b) {
+    return a / b;
+}
+
+
+function operate(a, b, operator) {
+    let result = 0;
+    switch (operator) {
+        case '+':
+            result = add(a, b);
+            break;
+        case '-':
+            result = subtract(a, b);
+            break;
+        case '*':
+            result = multiply(a, b);
+            break;
+        case '/':
+            result = divide(a, b);
+            break;
+    }
+    return result;
+}
