4518e4ade155ee034aaf780cdbd0f6cbe2b03529
Mathboss42 <115639944+Mathboss42@users.noreply.github.com>
2022-11-12 10:01:27 +0100

    Add backspace and 'invert sign' features

--- script.js
+++ script.js
@@ -3,18 +3,22 @@ let firstValue = '';
 let secondValue = '';
 let currentOperator = '';
 let isDone = false;
+let isOperatorPressed = false;
 
 
 const displayScreen = document.querySelector('p');
 const operandButtons = Array.from(document.querySelectorAll('.operand'));
 operandButtons.forEach(button => button.addEventListener('click', displayDigit));
 const operatorButtons = Array.from(document.querySelectorAll('.operator'));
-operatorButtons.splice(3, 2);
 operatorButtons.forEach(button => button.addEventListener('click', setOperator));
 const equalsButton = document.querySelector('[data-key="="]');
 equalsButton.addEventListener('click', equals);
 const clearButton = document.querySelector('[data-key="clear"]');
 clearButton.addEventListener('click', clear);
+const backButton = document.querySelector('[data-key="back"]');
+backButton.addEventListener('click', erase);
+const invertButton = document.querySelector('[data-key="+/-"]');
+invertButton.addEventListener('click', invertSign);
 
 
 //when user presses operator, store display value in temporary variable and reset display value
@@ -24,7 +28,9 @@ clearButton.addEventListener('click', clear);
 
 
 function displayDigit(event) {
-    if (isDone) {
+    if (isOperatorPressed) {
+        isOperatorPressed = false;
+    } else if (isDone) {
         clear();
         isDone = false;   
     } 
@@ -36,13 +42,6 @@ function displayDigit(event) {
         displayValue += event.target.dataset.key;
         displayScreen.textContent = displayValue;
     }
-    if (firstValue !== '' && secondValue === '') {
-        secondValue = displayValue;
-        // console.log(firstValue);
-        // console.log(secondValue);
-        // console.log(displayValue);
-        // console.log(currentOperator);
-    }
 }
 
 
@@ -52,20 +51,24 @@ function updateDisplay() {
 
 
 function setOperator(event) {
+    if (isOperatorPressed === true) return;
     if (firstValue === '' && secondValue === '') {
         firstValue = displayValue;
         displayValue = '0';
         currentOperator = event.target.dataset.key;
+        isOperatorPressed = true;
         // console.log(firstValue);
         // console.log(secondValue);
         // console.log(displayValue);
         // console.log(currentOperator);
-    } else if (firstValue !== '' && secondValue !== '') {
+    } else if (firstValue !== '') {
         equals(false);
         currentOperator = event.target.dataset.key;
         firstValue = displayValue;
         secondValue = '';
         displayValue = '0';
+        isOperatorPressed = true;
+        isDone = false;
         // console.log(firstValue);
         // console.log(secondValue);
         // console.log(displayValue);
@@ -75,9 +78,18 @@ function setOperator(event) {
 
 
 function equals(status = true) {
-    
+    if (isDone) return;
+    if (firstValue !== '' && secondValue === '') {
+        secondValue = displayValue;
+        // console.log(firstValue);
+        // console.log(secondValue);
+        // console.log(displayValue);
+        // console.log(currentOperator);
+    }
     if (firstValue !== '' && secondValue !== '') { //check if all operands have been entered, causes bugs if no check
         displayValue = operate(firstValue, secondValue, currentOperator);
+        firstValue = displayValue;
+        secondValue = '';
         updateDisplay();
         isDone = status;
     } else { //otherwise don't do anything
@@ -91,6 +103,40 @@ function clear() {
     firstValue = '';
     secondValue = '';
     updateDisplay();
+}
+
+
+function erase() {
+    if (isDone) return;
+    if (displayValue === '0') {
+        // console.log('displayval 0');
+        return;
+    } else if (displayValue.length === 1) {
+        if (true) {
+            // console.log('displayval ' + displayValue);
+            // console.log('display length ' + displayValue.length)
+            displayValue = '0';
+            updateDisplay();
+        }
+    } else {
+        // console.log('displayval ' + displayValue);
+        // console.log('slice');
+        displayValue = String(displayValue).slice(0, -1);
+        updateDisplay()
+    }
+}
+
+
+function invertSign() {
+    if (displayValue == 0) return;
+    if (displayValue > 0) {
+        displayValue = -displayValue;
+        updateDisplay();
+    } else {
+        console.log('display val ' + String(displayValue) + typeof displayValue);
+        displayValue = String(displayValue).substring(1)
+        updateDisplay();
+    }
 }
 
 
@@ -114,6 +160,11 @@ function divide(a, b) {
         return "Nice try, but no."
     }
     return Math.round(a / b * 10) / 10;
+}
+
+
+function modulo () {
+    return;
 }
 
 
